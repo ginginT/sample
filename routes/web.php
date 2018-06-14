b2cd473648b3909ffc183fb86e98759fc8b95c0e
gingin <X_SJian@163.com>
2018-06-14 16:12:46 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ Route::get('/', 'StaticPagesController@home')->name("home");
 Route::get('/help', 'StaticPagesController@help')->name("help");
 Route::get('/about', 'StaticPagesController@about')->name("about");
 
-Route::get('signup','UsersController@create')->name('signup');
-Route::resource('users','UsersController');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
 
-Route::get('login','SessionsController@create')->name('login');
-Route::post('login','SessionsController@store')->name('login');
-Route::delete('logout','SessionsController@destroy')->name('logout');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
@@ -28,4 +30,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-Route::resource('statuses','StatusesController',['only' => ['store', 'destroy']]);
+Route::resource('statuses', 'StatusesController',['only' => ['store', 'destroy']]);
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
